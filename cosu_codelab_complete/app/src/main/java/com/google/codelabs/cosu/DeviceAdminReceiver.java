05e2b9bcd564cbf9bdbb17fe9ea5b878e1df10e7
Natanel Fuchs <nyfuchs@google.com>
2016-05-09 17:34:44 -0400

    change package name

---
+++ cosu_codelab_complete/app/src/main/java/com/google/codelabs/cosu/DeviceAdminReceiver.java
@@ -0,0 +1,23 @@
+package com.google.codelabs.cosu;
+
+/**
+ * Created by nyfuchs on 4/26/16.
+ */
+
+import android.content.ComponentName;
+import android.content.Context;
+
+/**
+ * Handles events related to the managed profile.
+ */
+public class DeviceAdminReceiver extends android.app.admin.DeviceAdminReceiver {
+    private static final String TAG = "DeviceAdminReceiver";
+
+    /**
+     * @param context The context of the application.
+     * @return The component name of this component in the given context.
+     */
+    public static ComponentName getComponentName(Context context) {
+        return new ComponentName(context.getApplicationContext(), DeviceAdminReceiver.class);
+    }
+}
